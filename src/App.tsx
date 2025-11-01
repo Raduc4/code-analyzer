9295f5e99863ac375234991f66087b1811e25353
Radu Cazacu <cazacuraduic@gmail.com>
2025-11-01 11:19:19 +0200

    Project scaffold and initialization

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./global.css";
+
+function App() {
+ 
+
+  return (
+    <main className="container w-10 h-10 bg-amber-300">
+     
+    </main>
+  );
+}
+
+export default App;
